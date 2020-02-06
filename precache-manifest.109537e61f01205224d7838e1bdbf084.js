856f8b2a985dfdce1ba9d5b598abcc9eb13077ea
Marina <marfen24@i.ua>
2020-02-06 21:38:45 +0200

    Updates

---
+++ precache-manifest.109537e61f01205224d7838e1bdbf084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5182fbed6dd60cec4e1535f35fa424",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "d3928bcae5d98b460bf3",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.9c4aa509.chunk.css"
+  },
+  {
+    "revision": "7b18d92ae04bca7f984e",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.d2e5d6fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.d2e5d6fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3928bcae5d98b460bf3",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.a4c8d9a5.chunk.js"
+  },
+  {
+    "revision": "20af662a8729a8db5d69",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.7aee43a4.js"
+  }
+]);
